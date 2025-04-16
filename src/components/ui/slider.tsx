32038269c94d2bc015b67d8a0c50591f5e0fcc2f
Push Tech <pushtechja@gmail.com>
2025-04-16 23:42:52 +0000

    slider thumb should be emoji.

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -20,9 +20,14 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+     <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0">
+          <span className="slider-emoji">
+            {props.value && (props.value[0] <= 33 ? '☹️' : props.value[0] <= 66 ? '😐' : '🥳')}
+          </span>
+        </SliderPrimitive.Thumb>
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
 
 export { Slider }
+
